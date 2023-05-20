95645bf868b198da44a749f261aa7e0638684f99
STEPHAN <83315377+RENSOG@users.noreply.github.com>
2023-05-20 13:38:48 +0530

    app updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import Food from "./Food";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Food from "./Food.jsx";
+import LoginForm from "./Pages/Login";
+import Register from "./Pages/Register.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <Food/>
-    </div>
+    <BrowserRouter>
+      {" "}
+      <Routes>
+        
+       
+            <Route exact path="/" element={<LoginForm/> }/>
+            <Route path="/food" element={<Food/>} />
+            <Route path="/register" element={<Register/>} />
+      
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
